8c4133b6049c1c66378f4977fdc4987dca29067b
seaona <mariona@gmx.es>
2022-09-18 09:19:23 +0200

    remove console logs

--- src/main/shell.js
+++ src/main/shell.js
@@ -8,29 +8,27 @@ function isInstalled(dapp) {
     const dataString = data.toString();
 
     if (!`${data}`) {
-      console.log(false);
       return false;
     }
     if (data.toString().includes(dappsData[dapp].folder)) {
-      console.log(true);
       return true;
     }
   });
 }
 
 async function install(dapp) {
-  console.log('Cloning the project');
+  console.log('Cloning the project...');
   await spawnSync('git', ['clone', dappsData[dapp].codebase], {
     cwd: 'local-app',
   });
-  console.log('Installing dependencies');
+  console.log('Installing dependencies...');
   await spawnSync('yarn', ['install'], {
     cwd: `local-app/${dappsData[dapp].folder}`,
   });
 }
 
 async function start(dapp) {
-  console.log('Starting project');
+  console.log('Starting project...');
   await spawnSync('yarn', ['start'], {
     detached: true,
     cwd: `local-app/${dappsData[dapp].folder}`,
@@ -40,10 +38,8 @@ async function start(dapp) {
 async function runLocalApp(dapp) {
   const installed = isInstalled(dapp);
   if (installed) {
-    console.log('installed');
     await start(dapp);
   } else {
-    console.log('not installed');
     await install(dapp);
     await start(dapp);
   }
