ddc24a86a4aa6879cae4e5f043cb0c45540a3ba6
liang3030 <liangzhang3030@gmail.com>
2022-09-18 08:17:38 +0200

    UI

--- src/view/Dapps.tsx
+++ src/view/Dapps.tsx
@@ -1,25 +1,24 @@
 // import 'tailwindcss/tailwind.css';
 import Cards from './Card';
 
-const menus = ['App Store', 'Settings'];
 const Dapps = () => {
   return (
     <div className="container flex flex-col h-screen">
       <div className="flex flex-row flex-wrap  h-full">
         <aside className="w-1/4 px-2 border-r-2">
-          <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
-            <div className="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
+          <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased text-gray-800">
+            <div className="fixed flex flex-col top-0 left-0 w-full h-full border-r">
               <div className="overflow-y-auto overflow-x-hidden flex-grow">
                 <ul className="flex flex-col py-4 space-y-1">
                   <li className="px-5">
                     <div className="flex flex-row items-center h-8">
-                      <div className="text-sm font-light tracking-wide text-gray-600 font-medium">
+                      <div className="text-sm tracking-wide text-gray-600 font-medium">
                         My Local Dapp Store
                       </div>
                     </div>
                   </li>
                   <li>
-                    <div className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                    <div className="relative flex flex-row items-center h-11 focus:outline-none text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
                       <span className="inline-flex justify-center items-center ml-4">
                         <svg
                           className="w-5 h-5"
@@ -42,7 +41,7 @@ const Dapps = () => {
                     </div>
                   </li>
                   <li>
-                    <div className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                    <div className="relative flex flex-row items-center h-11 focus:outline-none text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
                       <span className="inline-flex justify-center items-center ml-4">
                         <svg
                           className="w-5 h-5"
